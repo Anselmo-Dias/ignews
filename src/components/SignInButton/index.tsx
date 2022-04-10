8ae0052c8e085294f470326a9fe011e04731d858
Anselmo <anselmodiasyahoo@gmail.com>
2022-04-09 22:08:48 -0300

    comit

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,8 +1,21 @@
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+
+import styles from "./styles.module.scss";
 
 export function SignInButton() {
-    return (
-        <button type="button">
-            Sign in with GitHub
-        </button>
-    );
+  const userIslogged = false;
+
+  return userIslogged ? (
+    <button className={styles.signInButton} type="button">
+      <FaGithub color="#04d361" />
+      Anselmo Alves
+      <FiX className={styles.closeImg} color="#737380" />
+    </button>
+  ) : (
+    <button className={styles.signInButton} type="button">
+      <FaGithub color="#eba417" />
+      Sign in with GitHub
+    </button>
+  );
 }
